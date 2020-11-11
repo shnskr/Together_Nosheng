5e55c3e5b7cd3e6dead3a892d5e679c00cd41244
sol <solsolahn@gmail.com>
2020-11-11 10:15:24 +0900

    회원비번 재설정 수정

--- Nolgo10jo/app/src/main/java/com/together/nosheng/MemberInfoActivity.java
+++ Nolgo10jo/app/src/main/java/com/together/nosheng/MemberInfoActivity.java
@@ -12,76 +12,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class MemberInfoActivity extends AppCompatActivity {
-    private Button join, login, reset;
-    private EditText email_login;
-    private EditText pwd_login;
+    private Button info_submit;
+
     private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_member_info);
 
-        join = (Button) findViewById(R.id.join);
-        login = (Button) findViewById(R.id.login);
-        reset = (Button) findViewById(R.id.Info_Submit);
-        email_login = (EditText) findViewById(R.id.email);
-        pwd_login = (EditText) findViewById(R.id.log_password);
+        info_submit = (Button) findViewById(R.id.Info_Submit);
 
         firebaseAuth = firebaseAuth.getInstance();
 
-        login.setOnClickListener(new View.OnClickListener() {
+        info_submit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String email = email_login.getText().toString().trim();
-                String pwd = pwd_login.getText().toString().trim();
-
-                if(email.length()>0 && pwd.length()>0) {
-
-                    firebaseAuth.signInWithEmailAndPassword(email, pwd)
-                            .addOnCompleteListener(MemberInfoActivity.this, new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
-                                        startToast("로그인 성공!");
-                                        startMyActivity(MainActivity.class);
-                                    } else {
-                                        startToast(task.getException().toString());
-                                    }
-                                }
-                            });
-                }else {
-                    if(email.length()==0) {
-                        startToast("이메일을 입력해주세요");
-                    }
-                    if(pwd.length()==0){
-                        startToast("패스워드를 입력해주세요");
-                    }
-                }
-
-            }
+            public void onClick(View v) { profileUpdate(); }
         });
-
-
-        join.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startMyActivity(RegisterActivity.class);
-            }
-        });
-
-        reset.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startMyActivity(PasswordResetActivity.class);
-            }
-        });
-
-
     }
 
     private void startMyActivity(Class c){
@@ -90,4 +42,31 @@ public class MemberInfoActivity extends AppCompatActivity {
     }
 
     private void startToast(String msg) { Toast.makeText(this, msg, Toast.LENGTH_LONG).show();}
+
+    private void profileUpdate() {
+        String name = ((EditText)findViewById(R.id.UserName)).getText().toString();
+
+        if(name.length()>0) {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                    .setDisplayName("Jane Q. User")
+                    .build();
+
+            user.updateProfile(profileUpdates)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                startToast("업데이트 완료");
+                            }
+                        }
+                    });
+        }else {
+            startToast("이름을 입력해주세요");
+        }
+    }
+
 }
+
+
--- Nolgo10jo/app/src/main/java/com/together/nosheng/PasswordResetActivity.java
+++ Nolgo10jo/app/src/main/java/com/together/nosheng/PasswordResetActivity.java
@@ -1,10 +1,12 @@
 package com.together.nosheng;
 
+
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -12,27 +14,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class PasswordResetActivity extends AppCompatActivity {
-    private Button info_submit;
-    private FirebaseAuth firebaseAuth;
+    private Button pw_reset;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password_reset);
+        mAuth = FirebaseAuth.getInstance();
+        pw_reset = (Button) findViewById(R.id.pw_reset);
 
-        info_submit = (Button) findViewById(R.id.Info_Submit);
 
-        firebaseAuth = firebaseAuth.getInstance();
-
-        info_submit.setOnClickListener(new View.OnClickListener() {
+        pw_reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                userUpdate();
+                resetPW();
             }
         });
 
@@ -45,20 +47,15 @@ public class PasswordResetActivity extends AppCompatActivity {
 
     private void startToast(String msg) { Toast.makeText(this, msg, Toast.LENGTH_LONG).show();}
 
-    private void userUpdate () {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private void resetPW () {
+        String emailAddress = ((EditText)findViewById(R.id.email_reset)).getText().toString();
 
-        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                .setDisplayName("Jane Q. User")
-                .setPhotoUri(Uri.parse("https://example.com/jane-q-user/profile.jpg"))
-                .build();
-
-        user.updateProfile(profileUpdates)
+        mAuth.sendPasswordResetEmail(emailAddress)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
-                            startToast("업데이트 완료");
+                            startToast("재설정 이메일 전송 완료");
                         }
                     }
                 });
