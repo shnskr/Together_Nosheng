3504407b31ac1f0473bdad3b567acc3ce3a28e22
sungjinkim <0ksj340@gmail.com>
2020-11-26 17:19:36 +0900

    adapter 에 사용할 layout 추가 및 코드 정리

--- Nolgo10jo/app/src/main/java/com/together/nosheng/view/HomeFragmentActivity.java
+++ Nolgo10jo/app/src/main/java/com/together/nosheng/view/HomeFragmentActivity.java
@@ -6,33 +6,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.together.nosheng.R;
 import com.together.nosheng.databinding.ActivityFragmentHomeBinding;
-import com.together.nosheng.model.project.Post;
-import com.together.nosheng.viewmodel.HomeAdapter;
-
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class HomeFragmentActivity extends Fragment {
 
     private ActivityFragmentHomeBinding homeBinding;
-    private HomeAdapter homeAdapter;
-    FloatingActionButton btn;
 
     @Nullable
     @Override
@@ -41,16 +26,13 @@ public class HomeFragmentActivity extends Fragment {
         homeBinding = ActivityFragmentHomeBinding.inflate(inflater, container, false);
         View view = homeBinding.getRoot();
 
-        Log.i("aptpsldkmfwlekf", "sdfsdfs");
         homeBinding.btnNewTrip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("ㅁㄴㅇㅁ", "ㅂㅂㅂ");
                 Intent intent = new Intent(getActivity(), BottomBtnActivity.class);
                 startActivity(intent);
             }
         });
-        Log.i("aptpsldkmfwlekf", "sdfsd654132fs");
         return view;
     }
 }
