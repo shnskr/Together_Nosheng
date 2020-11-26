e2997addeab9a27f325900a1ddcdd04ad5471c00
dal <res1206@naver.com>
2020-11-26 17:14:52 +0900

    파일 삭제

--- Nolgo10jo/app/src/main/java/com/together/nosheng/view/HomeFragmentActivity.java
+++ Nolgo10jo/app/src/main/java/com/together/nosheng/view/HomeFragmentActivity.java
@@ -6,26 +6,13 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.together.nosheng.R;
 import com.together.nosheng.databinding.ActivityFragmentHomeBinding;
-import com.together.nosheng.model.project.Post;
-import com.together.nosheng.viewmodel.HomeAdapter;
-
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class HomeFragmentActivity extends Fragment {
