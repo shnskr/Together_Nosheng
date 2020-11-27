6616acb0f96c74a8700baaca1f161036a4e67d98
JungMin <jjmin9797@naver.com>
2020-11-27 16:05:00 +0900

    설정페이지 추가

--- Nolgo10jo/app/src/main/java/com/together/nosheng/model/user/User.java
+++ Nolgo10jo/app/src/main/java/com/together/nosheng/model/user/User.java
@@ -1,12 +1,32 @@
 package com.together.nosheng.model.user;
 
-import java.util.Date;
+import com.google.type.Date;
+import com.google.type.DateTime;
 
 public class User {
     private String eMail;
     private String nickName;
     private Date regDate;
     private String thumbnail;
+
+//
+    public User(String eMail, String nickName, Date regDate, String thumbnail) {
+        this.eMail = eMail;
+        this.nickName = nickName;
+        this.regDate = regDate;
+        this.thumbnail = thumbnail;
+    }
+
+
+
+
+    public User (String nickName){
+        this.nickName = nickName;
+    }
+    public User() {
+
+    }
+
 
     public String geteMail() {
         return eMail;
--- Nolgo10jo/app/src/main/java/com/together/nosheng/viewmodel/UserViewModel.java
+++ Nolgo10jo/app/src/main/java/com/together/nosheng/viewmodel/UserViewModel.java
@@ -1,20 +1,48 @@
 package com.together.nosheng.viewmodel;
 
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.google.firebase.auth.FirebaseUser;
+import com.together.nosheng.model.user.User;
 import com.together.nosheng.repository.FirebaseAuthDao;
 import com.together.nosheng.repository.FirebaseAuthDaoImpl;
+import com.together.nosheng.repository.UserRepository;
 
 public class UserViewModel extends ViewModel {
     private FirebaseAuthDao dao;
-
+    public LiveData<User> liveUser;
     public LiveData<FirebaseUser> firebaseUser;
+    private UserRepository userRepository;
 
     public UserViewModel() {
         dao = new FirebaseAuthDaoImpl();
         getFirebaseUser();
+        userRepository = new UserRepository(firebaseUser.getValue().getUid());
+        this.liveUser = userRepository.findAll();
+    }
+
+    public UserViewModel(boolean firstLogin) {
+        if (!firstLogin){
+
+        }
+    }
+
+
+
+
+    public void changeNickname(String value) {
+        userRepository.changeNickname(value);
+    }
+
+
+
+    public LiveData<User> userModelLiveData() {
+        return liveUser;
     }
 
     public void getFirebaseUser() {
