014d553618f31c5c47c126c3001a7cb69050b9d7
dal <res1206@naver.com>
2020-12-07 16:39:56 +0900

    승환이 관리자 페이지 추가

--- Nolgo10jo/app/src/main/java/com/together/nosheng/repository/FirebaseAuthDao.java
+++
@@ -1,9 +0,0 @@
-package com.together.nosheng.repository;
-
-import androidx.lifecycle.MutableLiveData;
-
-import com.google.firebase.auth.FirebaseUser;
-
-public interface FirebaseAuthDao {
-    MutableLiveData<FirebaseUser> getFirebaseUser();
-}
