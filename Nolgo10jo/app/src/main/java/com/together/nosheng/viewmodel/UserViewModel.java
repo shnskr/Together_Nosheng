5b74546a9a531bd7cb53861908f336bea1f10e38
sol <solsolahn@gmail.com>
2020-12-19 15:32:12 +0900

    세팅 관련 작업

--- Nolgo10jo/app/src/main/java/com/together/nosheng/viewmodel/UserViewModel.java
+++ Nolgo10jo/app/src/main/java/com/together/nosheng/viewmodel/UserViewModel.java
@@ -14,6 +14,7 @@ import com.together.nosheng.util.GlobalApplication;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class UserViewModel extends ViewModel {
     private LiveData<User> liveUser;
@@ -23,6 +24,7 @@ public class UserViewModel extends ViewModel {
     private LiveData<ArrayList<String>> friendNickName;
 
     private LiveData<List<String>> friendNickNamelist;
+    private LiveData<List<User>> userFriendList;
 
     String TAG = "User ViewModel : ";
 
@@ -65,4 +67,13 @@ public class UserViewModel extends ViewModel {
         friendNickNamelist =userRepository.getFriendList();
         return friendNickNamelist;
     }
+
+    public LiveData<List<User>> getUserFriendList(){
+        return userFriendList;
+    }
+
+    public void setUserFriendList(){
+        userFriendList = userRepository.getUserFriendList();
+    }
+
 }
