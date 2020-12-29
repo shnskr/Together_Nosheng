0c0a0d1fc0684a19ab1359d56b41bc60181eaf66
sol <solsolahn@gmail.com>
2020-12-29 17:28:15 +0900

    좋아요 수정

--- Nolgo10jo/app/src/main/java/com/together/nosheng/view/SearchPlanDetailActivity.java
+++ Nolgo10jo/app/src/main/java/com/together/nosheng/view/SearchPlanDetailActivity.java
@@ -57,7 +57,7 @@ public class SearchPlanDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = LayoutSearchItemDetailBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        String key = getIntent().getStringExtra("Key");
+        String key = getIntent().getStringExtra("Key"); //플랜 KEY
 
         userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
         userViewModel.setLiveUser();
@@ -67,30 +67,34 @@ public class SearchPlanDetailActivity extends AppCompatActivity {
                 tempUser = user;
             }
         });
-
+        binding.searchDetailLikeBtn.setBackgroundResource(R.drawable.ic_button_favorite_border_foreground);
 
 
         planViewModel = new ViewModelProvider(this).get(PlanViewModel.class);
         planViewModel.setPlanRepository();
         planViewModel.getPlans().observe(this, new Observer<Map<String, Plan>>() {
-                    @Override
-                    public void onChanged(Map<String, Plan> stringPlanMap) {
-                        planMap.putAll(stringPlanMap);
-                        plan= stringPlanMap.get(key);
-                        binding.searchDetailTitle.setText(plan.getPlanTitle());
-                        binding.searchDetailTheme.setText(plan.getPlanTheme());
-                        binding.searchDetailLike.setText(Integer.toString(plan.getPlanLike().size()));
-                    }
-                });
-
-        binding.searchDetailLikeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                userLike(key);
+            public void onChanged(Map<String, Plan> stringPlanMap) {
+                planMap.putAll(stringPlanMap);
+                plan = stringPlanMap.get(key);
+                binding.searchDetailTitle.setText(plan.getPlanTitle());
+                binding.searchDetailTheme.setText(plan.getPlanTheme());
+                binding.searchDetailLike.setText(Integer.toString(plan.getPlanLike().size()));
+                userCheck();
             }
         });
 
 
+
+        binding.searchDetailLikeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(backgrnd==0){ //좋아요를 누르지 않았을때혀ㅏㅏ
+                    userLike(key);
+                }
+                userCheck();
+            }
+        });
 
 
         if (key != null) {
@@ -98,6 +102,8 @@ public class SearchPlanDetailActivity extends AppCompatActivity {
             binding.searchBookmark.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+
+
                     getUserBookmark();
                     //addBookmark(key);
                     if (!(bookmarkID.contains(key))) {
@@ -108,20 +114,20 @@ public class SearchPlanDetailActivity extends AppCompatActivity {
                 }
 
             });
-        } else{
+        } else {
             Toast.makeText(getApplicationContext(), "이미 북마크에 추가되어 있습니다", Toast.LENGTH_LONG).show();
         }
     }
 
-    public void bookmark (String key){
-        Map <String, List<String>> bookmarkList = new HashMap<>();
-        if (bookmarkID != null){
-                bookmarkID.add(key);
+    public void bookmark(String key) {
+        Map<String, List<String>> bookmarkList = new HashMap<>();
+        if (bookmarkID != null) {
+            bookmarkID.add(key);
 
         }
         bookmarkList.put("bookmarkList", bookmarkID);
 
-        db.collection("User").document(GlobalApplication.firebaseUser.getUid()).set(bookmarkList,SetOptions.merge())
+        db.collection("User").document(GlobalApplication.firebaseUser.getUid()).set(bookmarkList, SetOptions.merge())
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
@@ -130,7 +136,7 @@ public class SearchPlanDetailActivity extends AppCompatActivity {
     }
 
 
-        public void getUserBookmark() {
+    public void getUserBookmark() {
         bookmarkID.clear();
         bookmarkID.addAll(tempUser.getBookmarkList());
 
@@ -144,21 +150,30 @@ public class SearchPlanDetailActivity extends AppCompatActivity {
 //                }
 //            }
 //        });
-        }
+    }
 
-        public void userLike (String key) {
+    public void userLike(String key) {
+        Log.i("detailActivity", " 실행중");
         List<String> planLike = new ArrayList<>();
-        for(String s : plan.getPlanLike()){
-        if (s.equals(GlobalApplication.firebaseUser.getUid())) {
-            Log.i("detailActivity", " 이미 좋아요 눌렀음");
-        }else {
-            if (key != null) {
-                planLike.addAll(planMap.get(key).getPlanLike());
-                planViewModel.userLiked(planLike, key);
+        Log.i("detailActivity", plan.getPlanTitle());
+
+
+
+        if (plan.getPlanLike().size() == 0) { //첫 라이크
+            planLike.addAll(planMap.get(key).getPlanLike()); // 모든 플랜을 불러와서 특정 플랜의 플랜 라이크 불러오기
+            planViewModel.userLiked(planLike, key);
+        } else { //그다음 라이크
+            if (plan.getPlanLike().contains(GlobalApplication.firebaseUser.getUid())){
+                Log.i("detailActivity", " 이미 좋아요 눌렀음");
+            } else {
+                if (key != null) { //키 존재시,
+                    Log.i(" 시금치를 흔들어", "뽑빠ㅃ이 ");
+                    planLike.addAll(planMap.get(key).getPlanLike());
+                    planViewModel.userLiked(planLike, key);
+                }
             }
         }
-        }
-        }
+    }
 
     public void userCheck(){
         for (String s : plan.getPlanLike()){
@@ -170,4 +185,7 @@ public class SearchPlanDetailActivity extends AppCompatActivity {
             }
         }
     }
+
 }
+
+
